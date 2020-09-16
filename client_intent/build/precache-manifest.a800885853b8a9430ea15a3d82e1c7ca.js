1cad0d1db84cedda740bfe726bc936da4c04539a
adityayadav7 <adityay179@gmail.com>
2020-09-16 20:45:39 +0530

    removed material menu

---
+++ client_intent/build/precache-manifest.a800885853b8a9430ea15a3d82e1c7ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117503ef8b84119256411711f052fc98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "426798589f61cb504463",
+    "url": "/static/css/main.97fab512.chunk.css"
+  },
+  {
+    "revision": "4aed1b5d0056c7279784",
+    "url": "/static/js/2.3b91873a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3b91873a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426798589f61cb504463",
+    "url": "/static/js/main.d9dee0b4.chunk.js"
+  },
+  {
+    "revision": "c31d046254e883c8ba9a",
+    "url": "/static/js/runtime-main.34976c1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- client_intent/src/component/Dashboard.js
+++ client_intent/src/component/Dashboard.js
@@ -5,9 +5,7 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+
 import axios from 'axios';
 class Dashboard extends PureComponent {
     constructor(props) {
