30a87d3b352574a0270cd04688506622a9e7abc0
adityayadav7 <adityay179@gmail.com>
2020-09-18 12:33:24 +0530

    remove comment

--- Server/functions/controller/controller.js
+++ Server/functions/controller/controller.js
@@ -1,15 +1,11 @@
 const dialogflow = require('@google-cloud/dialogflow');
 const rr =require('../config/TrinyTask-12bead48582b.json')
-// Instantiates clients
 const projectId='trinytask-289705'
 const intentsClient = new dialogflow.IntentsClient({
     keyFilename:'config/TrinyTask-12bead48582b.json'
 });
 
 async function listIntents(req,res) {
-  // Construct request
-
-  // The path to identify the agent that owns the intents.
   const projectAgentPath = intentsClient.agentPath(projectId);
 
   console.log(projectAgentPath);
@@ -18,7 +14,6 @@ async function listIntents(req,res) {
     parent: projectAgentPath,
   };
 
-  // Send the request for listing intents.
   const [response] = await intentsClient.listIntents(request);
   let intents=[];
   response.forEach(intent => {
@@ -27,24 +22,8 @@ async function listIntents(req,res) {
       intents.push({
         info
       })
-  //   console.log('====================');
-  //   console.log(`Intent name: ${intent.name}`);
-  //   console.log(`Intent display name: ${intent.displayName}`);
-  //   console.log(`Action: ${intent.action}`);
-  //   console.log(`Root folowup intent: ${intent.rootFollowupIntentName}`);
-  //   console.log(`Parent followup intent: ${intent.parentFollowupIntentName}`);
-
-  //   console.log('Input contexts:');
-  //   intent.inputContextNames.forEach(inputContextName => {
-  //     console.log(`\tName: ${inputContextName}`);
-  //   });
-
-  //   console.log('Output contexts:');
-  //   intent.outputContexts.forEach(outputContext => {
-  //     console.log(`\tName: ${outputContext.name}`);
-  //   });
   });
-  // console.log(intents);
+ 
    
   res.status(200).json(intents)
 }
--- Server/functions/index.js
+++ Server/functions/index.js
@@ -7,17 +7,5 @@ app.use(bodyParser.json())
 const cors = require('cors')
 app.use(cors());
 const listIntents =require('./controller/controller')
-//const runSample =require('./controller/controller')
-app.get('/intent',listIntents);
-//app.get('/ee',runSample)
+app.get('/',listIntents);
 exports.api = functions.https.onRequest(app);
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// const ue=require('./route')
-// app.use('/api',ue);
